28707b901b36fb97ffe0c8cdbab53dee317fa1c3
Levi van Boxtel <le.van.boxtel@sintlucasedu.nl>
2022-11-26 23:12:33 +0100

    Fixen animations & bugs

--- Assets/Scripts/EndTrigger.cs
+++ Assets/Scripts/EndTrigger.cs
@@ -6,6 +6,6 @@ using UnityEngine.SceneManagement;
 public class EndTrigger : MonoBehaviour {
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		SceneManager.LoadScene("Menu");
 	}
 }
---
+++ Assets/Scripts/WalkController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private float moveInput;
+    private bool facingRight = true;
+    private static readonly int Speed = Animator.StringToHash("Speed");
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (rb.velocity.x == 0)
+        {
+            animator.SetFloat(Speed, 0);
+        }
+        else
+        {
+            animator.SetFloat(Speed, 1);
+        }
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+
+        if (rb.position.y < -75f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
